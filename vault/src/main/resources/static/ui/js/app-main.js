f49df3e3577743af881938d1f52a389f71e48fc2
Vimal Singh <vimal.singh@philips.com>
2017-07-31 19:24:48 +0530

    search added.

--- vault/src/main/resources/static/ui/js/app-main.js
+++ vault/src/main/resources/static/ui/js/app-main.js
@@ -16,7 +16,23 @@ wndApp.controller('hackCtrl', function($scope, $http) {
 		responseValue.success(function(data) {
 			$scope.timelines = data.response;
 		});
+	};
+	
+	
+$scope.filterTimelines = function(tag) {
+
+		
+		var responseValue = $http({
+			method : "GET",
+			url : "http://ec2-13-126-115-105.ap-south-1.compute.amazonaws.com:8282/api/v1/timeline?tags="+tag,
+			accept : "application/json"
+		});
+
+		responseValue.success(function(data) {
+			$scope.timelines = data.response;
+		});
 	}
+	
 	
 	getAllTimelines();
 });
